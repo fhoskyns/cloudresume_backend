2acc685237f75c96fe4e74f2f15d832558255903
fhoskyns <80152828+fhoskyns@users.noreply.github.com>
2021-03-16 21:57:26 +0000

    Add files via upload

---
+++ Lambda/app.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+import os
+
+# Initialize dynamodb boto3 object
+dynamodb = boto3.resource('dynamodb')
+# Set dynamodb table name variable from env
+ddbTableName = os.environ['databaseName']
+table = dynamodb.Table(ddbTableName)
+
+def lambda_handler(event, context):
+
+    # Atomic update an item in table or add if doesn't exist
+    ddbResponse = table.update_item(
+        Key={
+            "id": "user1"
+        },
+        UpdateExpression='ADD clicks :inc',
+        ExpressionAttributeValues={
+            ':inc': 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    # Return api response object
+    return {
+    'statusCode': 200,
+    'headers': {
+        "Access-Control-Allow-Origin": "*"
+    },
+    'body': json.dumps({'clicks': int(ddbResponse['Attributes']['clicks'])})
+}
