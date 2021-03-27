f4a7be2a6b17164329722d37e18cc0bee2507cb1
fhoskyns <fredhoskyns@gmail.com>
2021-03-27 12:35:06 +0000

    Updated app.py

--- Lambda/app.py
+++ Lambda/app.py
@@ -2,6 +2,8 @@ import json
 import boto3
 import os
 
+# Comment to check SAM
+
 # Initialize dynamodb boto3 object
 dynamodb = boto3.resource('dynamodb')
 # Set dynamodb table name variable from env
